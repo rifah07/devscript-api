8c463b5d7390c68166b2689a2c0fbb8cd98e5631
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-05-29 12:43:41 +0600

    feat: add post module

---
+++ src/posts/posts.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { Post, PostSchema } from './schemas/post.schema';
+import { PostsService } from './posts.service';
+import { PostsResolver } from './posts.resolver';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+  ],
+  providers: [PostsService, PostsResolver],
+  exports: [PostsService], // export for AiModule
+})
+export class PostsModule {}
