8333bc77206923525c3efdc04c278c6b05fed1d5
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-05-30 10:02:51 +0600

    feat: ADD POST CONTROLLER IN POST MODULE

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from './schemas/post.schema';
 import { PostsService } from './posts.service';
 import { PostsResolver } from './posts.resolver';
+import { PostsController } from './posts.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
   ],
   providers: [PostsService, PostsResolver],
+  controllers: [PostsController],
   exports: [PostsService], // export for AiModule
 })
 export class PostsModule {}
