1045824a2bffe7722f0d20d1efe661d925c70143
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-06-15 15:20:34 +0600

    feat: Update Users Module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,14 +4,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
+import { CommonModule } from '../common/common.module';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [
     // Register this module's Mongoose models.
     // Only this module (and modules that import UsersModule) can use User model.
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    CommonModule,
   ],
   providers: [UsersService, UsersResolver],
+  controllers: [UsersController],
   // Export UsersService so AuthModule can use it for login verification
   exports: [UsersService],
 })
