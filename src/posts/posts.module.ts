0dd01cd9e4d79e3dc0cfa1b30224c322697364b5
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-07-15 15:20:55 +0600

    feat: update post module and gql sche,a

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -7,12 +7,14 @@ import { PostsResolver } from './posts.resolver';
 import { PostsController } from './posts.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { FollowsModule } from '../follows/follows.module';
+import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
     NotificationsModule,
     FollowsModule, // (for getFollowerIds)
+    CategoriesModule, // (for getRelatedPosts)
   ],
   providers: [PostsService, PostsResolver],
   controllers: [PostsController],
