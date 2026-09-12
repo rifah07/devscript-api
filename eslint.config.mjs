60b5f06883343005ad1094641381e7d7f7ec34a2
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-09-12 19:26:53 +0600

    feat: update configuration to not show unnecessary bug for test files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default tseslint.config(
   {
@@ -38,6 +39,14 @@ export default tseslint.config(
         },
       ],
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.e2e-spec.ts'],
+    plugins: { jest: jestPlugin },
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      'jest/unbound-method': 'error', // catches REAL unbound-method bugs, ignores mocks correctly
     },
   },
 );
